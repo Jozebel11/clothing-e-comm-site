00e69ec6f12e0fb5074c0f306511637221ff54bf
jozebel11 <jjhardwicke@hotmail.co.uk>
2023-01-17 22:30:26 +0000

    Add Header component

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Image from "next/image"
+
+function header() {
+  return (
+    <header>
+        <div>
+            <div>
+                <Image />
+            </div>
+        </div>
+    </header>
+  )
+}
+
+export default header
