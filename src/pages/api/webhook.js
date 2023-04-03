4f7186e1920ade5615077ee270f589e00bffb236
jozebel11 <jjhardwicke@hotmail.co.uk>
2023-04-03 12:37:35 +0100

    update stripe key to string

--- src/pages/api/webhook.js
+++ src/pages/api/webhook.js
@@ -14,7 +14,7 @@ const app = !admin.apps.length
 
  //Establish connection to stripe
 
- const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+ const stripe = require('stripe')(`${process.env.STRIPE_SECRET_KEY}`);
 
  const endpointSecret = process.env.STRIPE_SIGNING_SECRET;
 
