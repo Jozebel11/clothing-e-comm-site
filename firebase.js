d6a332b532801ce4ea203df99e92335d5ac16103
jozebel11 <jjhardwicke@hotmail.co.uk>
2023-08-07 12:25:10 +0100

    update

--- firebase.js
+++ firebase.js
@@ -7,14 +7,14 @@ import { initializeApp } from "firebase/app";
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAjMPikXF8w4RAeM_Cx3a0wcE4Uxm8wz5E",
-    authDomain: "clothing-e-comm-site.firebaseapp.com",
-    projectId: "clothing-e-comm-site",
-    storageBucket: "clothing-e-comm-site.appspot.com",
-    messagingSenderId: "244947011765",
-    appId: "1:244947011765:web:f36bb0562b1271bf9096fc",
-    measurementId: "G-MQS1MGGLKD"
-  };
+  apiKey: process.env.FIREBASE_KEY,
+  authDomain: "clothing-e-comm-site.firebaseapp.com",
+  projectId: "clothing-e-comm-site",
+  storageBucket: "clothing-e-comm-site.appspot.com",
+  messagingSenderId: "244947011765",
+  appId: "1:244947011765:web:f36bb0562b1271bf9096fc",
+  measurementId: "G-MQS1MGGLKD"
+};
 
   const app = !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app();
 
